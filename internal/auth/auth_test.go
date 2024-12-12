e543845c347b66e08c61c905deeac74fda6e71db
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-12 10:39:26 -0300

    Add tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T){
+	got, err := GetAPIKey(http.Header{
+		"Authorization": []string{"ApiKey ASDFGHJKL"},
+	}) 
+	if err != nil {
+		log.Fatal(err)
+	}
+	want := "ASDFGHJKL"
+
+	if !reflect.DeepEqual(want, got){
+		t.Fatalf("I wanted this: %v, but i got this: %v", want, got)
+	}
+}
